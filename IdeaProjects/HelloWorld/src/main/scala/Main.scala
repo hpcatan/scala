93590adb7821655011be88cc610b20f66703231c
sam <3915208@qq.com>
2017-08-16 09:11:21 +0800

    new

--- IdeaProjects/HelloWorld/src/main/scala/Main.scala
+++ IdeaProjects/HelloWorld/src/main/scala/Main.scala
@@ -1,6 +1,22 @@
 object Main {
-  def main(args: Array[String]): Unit =
+  def main(args: Array[String]): Unit ={
+    class Point(var x: Int, var y: Int) {
+
+      def move(dx: Int, dy: Int): Unit = {
+        x = x + dx
+        y = y + dy
+      }
+
+      override def toString: String =
+        s"($x-$y)"
+    }
+
+    val point1 = new Point(2, 3)
+    //point1.x  // 2
+    println(point1)  // prints (x, y)
     println("Hello, Scala developer!")
+  }
+
 
   val list: List[Any] = List(
     "a string",
